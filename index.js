e49977b6d3839ce11a166c00fb38417798dd4baf
harryFBloch <harryfbloch@gmail.com>
2019-01-07 12:39:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let message
+ if (feet <= 400) {
+   message = "This one is on me!"
+ } else if (feet > 2000 && feet < 2500) {
+   message = 'I will gladly take your thirty bucks.'
+ } else if (feet > 2500) {
+   message ='No can do.'
+ }
+ return message
+}
+
+function ternaryCheckCity(city){
+
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+  case 'generous':
+    return "Thank you so much."
+    break;
+  case 'not as generous':
+     return 'Thank you.'
+    break;
+  default:
+  return  "Bye."
+  }
+}
