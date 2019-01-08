5a738ebf468b494b0bb966deeddb0596159ecf84
Doug Fitzgerald <dfitzge1@conncoll.edu>
2019-01-07 22:18:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet){
+  let cost;
+  if (feet <= 400) {
+    cost = "This one is on me!";
+  } else if (feet > 2000 & feet < 2500){
+    cost = "I will gladly take your thirty bucks."
+  } else {
+    cost = "No can do."
+  }
+  return cost;
+}
+
+function ternaryCheckCity(city) {
+  const bool = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return bool
+}
+
+function switchOnCharmFromTip(tip){
+  let charm
+  switch(tip){
+    case "generous":
+      charm = "Thank you so much.";
+      break;
+    case "not as generous":
+      charm = "Thank you.";
+      break;
+    default:
+      charm = "Bye."
+      break;
+  }
+  return charm;
+}
